2ad23a29fb5632425cdfb5e2c1b34572a974d36c
Vadim Miheev <mnixav@gmail.com>
2022-01-19 18:26:15 +0300

    Completing of GET "rest/players/id" endpoint;

--- src/main/java/com/game/controller/RestPlayersController.java
+++ src/main/java/com/game/controller/RestPlayersController.java
@@ -4,11 +4,15 @@ import com.game.entity.Player;
 import com.game.repository.PlayersRepository;
 import com.game.service.PlayersJPACriteriaBuilder;
 import org.springframework.data.domain.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest")
@@ -35,5 +39,19 @@ public class RestPlayersController {
     @GetMapping("/players/count")
     public Long getPlayersCount(HttpServletRequest request) {
         return playersRepository.count(PlayersJPACriteriaBuilder.getSpecificationByHttpRequestParams(request));
+    }
+
+    @GetMapping("/players/{id}")
+    public Object getPlayerByID(@PathVariable Long id) {
+        if (id < 1) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Optional<Player> player = playersRepository.findById(id);
+        if (player.isPresent()) {
+            return player.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
